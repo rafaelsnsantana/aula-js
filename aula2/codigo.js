f63d3cbb8a94c61d6d7bdc63f9d3285c80ddf460
Rafael Santana <rafaelsn.santana@gmail.com>
2020-01-21 19:39:28 -0300

    Subindo alguns exercícios realizados em 16-01

---
+++ aula2/codigo.js
@@ -0,0 +1,59 @@
+//nessa parte, irá criar uma janela com a mensagem abaixo
+//alert("Seja bem-vindo!");
+//document.write("Aprendendo javascript");
+// console.log("Recado para o desenvolvedor");
+
+// let nome , idade; 
+
+// nome = "Rafael";
+// idade = "30";
+
+// console.log("Nome: " + nome);
+// console.log("Idade: " + idade);
+
+// /* OPERADORES ARITMÉTICOS */ 
+// console.log("10 + 2 = " + (10 + 2) ); // adição
+// console.log("10 - 2 = " + (10 - 2) ); // subtração
+// console.log("10 * 2 = " + 10 * 2); // multiplicação
+// console.log("10 / 2 = " + 10 / 2); // divisão
+// console.log("10 % 2 = " + 10 % 2); // resto da divisão
+
+// /* OPERADORES RELACIONAIS */
+// console.log("10 > 2 = " + (10 > 2) ); // maior
+// console.log("10 < 2 = " + (10 < 2) ); // menor
+// console.log("10 == 2 = " + (10 == 2) ); // igual
+// console.log("10 != 2 = " + (10 != 2) ); // diferente
+// console.log("10 + 2 = " + (10 + 2) ); // maior ou igual
+// console.log("10 <= 2 = " + (10 <= 2) ); // menor ou igual
+
+// /* OPERADORES LÓGICOS */
+// console.log ("(10 < 2) && (5 == 5) = " + ( (10 < 2) && (5 == 5) ) ); // Operador E
+// console.log ("(10 < 2) || (5 == 5) = " + ((10 < 2) || (5 == 5)) ); // Operador OU
+// console.log ("!(5 == 5) = " + !(5 == 5) ); // Operador NÂO
+
+// // let numero1 = prompt("Informe um número");  
+// // let numero2 = prompt("Informe outro número");
+// numero1 = parseFloat(numero1);
+// numero2 = parseFloat(numero2);
+
+// let total = numero1 + numero2;
+// console.log(total);
+
+// console.log(numero1 + numero2); 
+// console.log(numero1 - numero2);
+// console.log(numero1 * numero2);
+// console.log(numero1 / numero2);
+
+// console.log(typeof(numero1));
+// console.log(typeof(numero2));
+
+// let nota1 = prompt("Informe nota1");
+// let nota2 = prompt("Informe nota2");
+// let nota3 = prompt("Informe nota3");
+// let nota4 = prompt("Informe nota4"); 
+// let media = (nota1 + nota2 + nota3 + nota4) / 4;
+// console.log("A média do João é " + media);
+
+let a = 999;
+let b = 555;
+let c;
---
+++ exercicios/exercicio7.js
@@ -0,0 +1,8 @@
+let tempo = prompt ("Entre com o tempo de experiência");
+tempo = parseFloat(tempo);
+if (tempo < 2)
+console.log("Júnior")
+if else ( (tempo>2) && (tempo<5) )
+console.log("Pleno")
+else (tempo > 5)
+console.log("Sênior")
